73a48af8acbb7e959e2f6e6d6871a7a9a56a34e5
Replit user <>
2023-03-21 17:47:56 +0000

    new try

--- list.c
+++ list.c
@@ -50,11 +50,18 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
-    return NULL;
+    if(list==NULL || list->current==NULL || list->current->next==NULL)return NULL;
+
+    while(list->current->next!=NULL){
+      list->current=list->current->next;
+    }
+    return list->current->data;
 }
 
 void * prevList(List * list) {
-    return NULL;
+    if(list==NULL || list->current==NULL || list->current->next==NULL)return NULL;
+    list->current=list->current->prev;
+    return list->current->data;
 }
 
 void pushFront(List * list, void * data) {
