574ba6ca731f3221da90c9e06aab1da2abc5babc
Replit user <>
2023-03-23 15:12:28 +0000

    new try

--- list.c
+++ list.c
@@ -87,7 +87,6 @@ void pushCurrent(List * list, void * data) {
     Node *n = createNode(data);
   
     if (list->current == NULL) {
-      list->head = n ;
       list->current = n ;
     } else {
       list->current->next = n ;
